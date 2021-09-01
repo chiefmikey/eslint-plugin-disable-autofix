9ea4a3c7a59525462a4645eba71d026064018867
chiefmikey <wolfemikl@gmail.com>
2021-08-31 21:40:46 -0600

    Rename directory

--- no-autofix-2/lib/index.js
+++
@@ -1,9 +0,0 @@
-import all from './configs/all';
-import rules from './rules';
-
-export default {
-  configs: {
-    all,
-  },
-  rules,
-};
--- no-autofix-2/lib/utils.js
+++
@@ -1,8 +0,0 @@
-import ruleComposer from 'eslint-rule-composer';
-
-export default (rule) => {
-  return ruleComposer.mapReports(
-    Object.create(rule),
-    (problem) => ((problem.fix = null), problem),
-  );
-};
